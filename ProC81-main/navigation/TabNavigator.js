24ba7fae50e4321a9ee99d76d3c083f5763df912
johanjince12 <96847741+johanjince12@users.noreply.github.com>
2022-08-05 17:27:50 -0500

    Add files via upload

---
+++ ProC81-main/navigation/TabNavigator.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import Feed from "../screens/Feed";
+import CreatePost from "../screens/CreatePost";
+
+const Tab = createBottomTabNavigator();
+
+const BottomTabNavigator = () => {
+    return (
+      <Tab.Navigator
+        labeled={false}
+        barStyle={styles.bottomTabStyle}
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName;
+            if (route.name === "Feed") {
+              iconName = focused ? "home" : "home-outline";
+            } else if (route.name === "Create Story") {
+              iconName = focused ? "add-circle" : "add-circle-outline";
+            }
+            return (
+              <Ionicons
+                name={iconName}
+                size={RFValue(25)}
+                color={color}
+                style={styles.icons}
+              />
+            );
+          }
+        })}
+        activeColor={"#ee8249"}
+        inactiveColor={"gray"}
+      >
+        <Tab.Screen name="Feed" component={Feed} />
+        <Tab.Screen name="Create Story" component={CreateStory} />
+      </Tab.Navigator>
+    );
+  };
+
+export default BottomTabNavigator
